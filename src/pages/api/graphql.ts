0c3a4a7dfcd31abdc4d6d9ba0ec76df9c2152629
Dennis vinterfjärd <dennis@vinterfjard.com>
2023-02-03 10:40:34 +0100

    Moving graphql defs out of pages folder

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import resolvers from '../graphql/resolvers';
-import typeDefs from '../graphql/typedefs';
+import resolvers from '@graphql/resolvers';
+import typeDefs from '@graphql/typedefs';
 
 const server = new ApolloServer({
     resolvers,
