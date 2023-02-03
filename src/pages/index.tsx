395636b6e17ce913c7fc59119d767443409110ab
Dennis vinterfjärd <dennis@vinterfjard.com>
2023-02-03 16:34:39 +0100

    Removing whitespace

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,10 +14,7 @@ export default function Home({ user }: { user: TUser }) {
 }
 
 export async function getStaticProps() {
-
-  // fetch data from an graphql api
   const { data } = await client.query(UserQueryGQL('2', ['id', 'name']));
-  
   return {
     props: {
       user: data.user
