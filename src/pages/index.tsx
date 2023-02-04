4b1cd42edafdc1141eac63c194bfc36019a9810f
Dennis vinterfjärd <dennis@vinterfjard.com>
2023-02-04 01:24:33 +0100

    Run userQuery after useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,11 @@ import { useEffect } from 'react';
 export default function Home({ user }: { user: TUser }) {
 
   const [mutateFunction, { data, loading, error }] = useMutation(CREATE_USER_GQL);
-  const {loading:listLoading, data:listData, error:listError} = useQuery(LIST_USER_GQL)
-
   useEffect(() => {
-    mutateFunction({variables: {name: 'test'}});
+     mutateFunction({variables: {name: 'test'}});
   }, []);
 
-  console.log(listData);
-
+  const {loading:listLoading, data:listData, error:listError} = useQuery(LIST_USER_GQL)
   return (
       <div>
         {listLoading && <p>Loading...</p>}
